93db0e4a04ff3ef56bc4c80088a5000f848ba6ac
Pranay Gaikwad <gpranay1021@gmail.com>
2023-05-22 16:48:20 +0530

    initial commit

---
+++ my-react-app/src/App.js
@@ -0,0 +1,27 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <p>hello</p>
+      <Interpolationbasics></Interpolationbasics>
+    </div>
+  );
+}
+
+function Interpolationbasics() {
+  let title = "hello universe";
+  let para = `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam libero,
+  aut iusto quam quasi earum corrupti dicta quis tempore eligendi,
+  repudiandae eum sit quae! Cum animi ratione architecto exercitationem
+  quam.`;
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{para}</p>
+    </div>
+  );
+}
+
+export default App;
